b33762f9fb34d2c0a975e35448199d4dc649dab5
Rezaes19 <iwcreza@gmail.com>
2026-08-30 14:13:36 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ def patch_peer_id_validation():
 patch_peer_id_validation()
 API_ID = 34996139
 API_HASH = "a1f3db16cae2919cfb05e61d1e968b8d"
-BOT_TOKEN = "8484770380:AAFG8PDYIwfRj3HD5QvZZtWwXuufisLTtXc" 
+BOT_TOKEN = "8858887304:AAELneONarg-zYTRBAWocRV9NO9xRzodFFg" 
 
 GOD_ADMIN_IDS = [6691993264, 7831049189]
 

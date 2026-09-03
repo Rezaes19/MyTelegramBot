c88a07649686f1ee982b1a77b93720a64ab610d5
Rezaes19 <iwcreza@gmail.com>
2026-09-03 13:59:22 +0330

    Update main.py

--- main.py
+++ main.py
@@ -218,7 +218,7 @@ def apply_telegram_style(text: str, style: str) -> str:
     elif style == "italic":
         return f"__{text}__"
     elif style == "quote":
-        return f"❝ {text} ❞"
+        return f"> {text}"  # ← این همون نقل قول دستی تلگرامه
     elif style == "strikethrough":
         return f"~~{text}~~"
     elif style == "underline":
